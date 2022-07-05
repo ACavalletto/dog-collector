1e5c21bd93fcca5ef22a31b0dd7d0ec920f89991
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-07-05 09:25:34 -0600

    Cat index page implemented

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# classes
+class Cat: 
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+        
+# dummy data
+cats = [
+    Cat('Lolo', 'tabby', 'foul little demon', 3),
+    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+    Cat('Raven', 'black tripod', '3 legged cat', 4)
+]
+
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'main_app/home.html')
 
 def about(request):
     return render(request, 'main_app/about.html')
+
+def cats_index(request):
+    return render(request, 'main_app/cats/index.html', {'cats': cats})
